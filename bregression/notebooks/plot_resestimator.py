39b5de1b24a0ae3f959f051edb7ebed74aaafe0f
Nadezda <nadezda.chernyavskaya@gmail.com>
2018-09-04 16:38:17 +0200

    plot for res estimator

--- bregression/notebooks/plot_resestimator.py
+++ bregression/notebooks/plot_resestimator.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#import ROOT
+import ROOT
 #from ROOT import TCanvas, TH1F
+
+matplotlib.rc_file('/users/nchernya/jupyter/bregression/matplotlibrc_LHCb')
+
 
 file = 'applied_res_2018-07-20_ttbar_full_RegressionPerJet_heppy_energyRings_testing_morevar.hd5'
 training ='2018-04-06_job23_2016'
 path = '/users/nchernya/HHbbgg_ETH/bregression/output_root/paper/'
 sample_name = 'ttbar'
+
 
 data = pd.read_hdf('%s%s'%(path,file))
 res = (data['Jet_resolution_NN_%s'%training])
@@ -15,13 +19,16 @@ res=np.array(res)
 plt.hist(res,bins=200,normed=1,histtype='stepfilled')
 axes = plt.gca()
 axes.set_xlim(0,0.3)
-plt.grid(alpha=0.2,linestyle='--',markevery=2)
+#plt.grid(alpha=0.2,linestyle='--',markevery=2)
 ymin, ymax = (axes).get_ylim()
 xmin, xmax = (axes).get_xlim()
 samplename='$t\\bar{t}$'
-plt.text(xmax*0.8,ymax*0.85,r'%s'%samplename, fontsize=30)
-plt.xlabel(r'$\hat{\sigma}$',fontsize=30)
-plt.ylabel('A.U.',fontsize=30)
+plt.text(xmax*0.8,ymax*0.85,r'%s'%samplename)
+plt.xlabel(r'$\hat{\sigma}$',ha='right', x=1)
+plt.ylabel('A.U.',ha='right', y=1)
+plt.minorticks_on()
+plt.text(0.1,11,r'\textbf{CMS} \textit{Work in Progress}',fontsize=14)
+plt.text(0.1,13, '   ', {'size': 28})
 savename='resolution_%s'%sample_name
 path2='/users/nchernya/HHbbgg_ETH/bregression/plots/paper_rootstyle/%s/'%sample_name
 plt.savefig(path2+savename+'.pdf')
