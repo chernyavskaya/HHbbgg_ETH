8221aa3e9cbb41076919641adef4450486305621
Nadezda Chernyavskaya (ETHZ) [nchernya] <nchernya@t3ui02.psi.ch>
2019-04-17 15:21:30 +0200

    root to hdf5

--- bregression/scripts/root_to_hd5_loop.py
+++ bregression/scripts/root_to_hd5_loop.py
@@ -18,12 +18,12 @@ import sys
 
 #out_dir = '/scratch/nchernya/HHbbgg/othersamples_morevar/'
 #in_dir = '/scratch/nchernya/HHbbgg/othersamples_morevar/'
-in_dir = 'root://t3dcachedb.psi.ch:1094//pnfs/psi.ch/cms/trivcat/store/user/nchernya/HHbbgg/b_regression/ttbar_2016_legacy_JECv11/'
+in_dir = 'root://t3dcachedb.psi.ch:1094//pnfs/psi.ch/cms/trivcat/store/user/nchernya/HHbbgg/b_regression/ttbar_2018_16April2019/'
 #in_dir = '/mnt/t3nfs01/data01/shome/nchernya/HHbbgg_ETH_devel/submit_jobs/'
 #out_dir = '/scratch/nchernya/HHbbgg/ttbar_2017_v2/'
 #in_dir = '/mnt/t3nfs01/data01/shome/nchernya/HHbbgg_ETH_devel/submit_jobs/2017/'
 #out_dir = '/scratch/nchernya/HHbbgg/ttbar_2017_v3_newJEC/'
-out_dir = '/scratch/nchernya/HHbbgg/ttbar_2016_legacy_JECv11/'
+out_dir = '/scratch/nchernya/HHbbgg/ttbar_2018_19April/'
 rings=[
                     'Jet_energyRing_dR0_em',
                     'Jet_energyRing_dR1_em',
@@ -52,13 +52,13 @@ for ring in rings:
     branch_names.append('%s_Jet_rawEnergy'%ring)
 cuts='(Jet_pt > 15) & (Jet_eta<2.5 & Jet_eta>-2.5) & (Jet_mcFlavour==5 | Jet_mcFlavour==-5) & (Jet_mcPt>1.) & (Jet_mcPt<6000)' #all for presentaion on 23 Jan was done with pT cut at 20 GeV
 
-file_list  = [94 , 18 , 2 , 55 , 16 , 40 , 97 , 35 , 37 , 98 , 7 , 31 , 51 , 45 , 8 , 65 , 41 , 56 , 83 , 89 , 69 , 99 , 88 , 79 , 21 , 12 , 17 , 3 , 30 , 32 , 26 , 27 , 70 , 73 , 13 , 92 , 59 , 93 , 64 , 100 , 22 , 46 , 50 , 78 , 84 , 54 , 60 , 20 , 36 , 11 , 75 , 74 , 149 , 124 , 129 , 142 , 114 , 147 , 148 , 109 , 138 , 115 , 120 , 144 , 106 , 133 , 104 , 110 , 152 , 153 , 101 , 125 , 105 , 134 , 119 , 128 , 139 , 143 , 123 , 25 , 113 , 44 , 132 , 63 , 151 , 118 , 82 , 49 , 137 , 68 , 87 , 1 , 6 , 10 , 15 , 103 , 34 , 122 , 53 , 141 , 108 , 72 , 39 , 127 , 91 , 58 , 146 , 77 , 96 , 24 , 112 , 43 , 131 , 62 , 150 , 29 , 117 , 81 , 48 , 136 , 67 , 155 , 86 , 5 , 14 , 102 , 33 , 121 , 52 , 140 , 19 , 107 , 71 , 38 , 126 , 90 , 57 , 145 , 76 , 95 , 23 , 111 , 42 , 130 , 61 , 28 , 116 , 80 , 47 , 135 , 66 , 154 , 85 , 4 , 9 ] 
+file_list  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,  124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157]
 #filelist=['ZHbbll_RegressionPerJet_heppy_energyRings_forTesting']
 #filelist=['HHsm','HHres500','HHres700','ZHbbll']
-for i in range(35,len(file_list)):
+for i in range(0,len(file_list)):
 #for i in range(start,end):
   num = file_list[i]
-  filename='TTbar_legacy2016_JECv11_RegressionPerJet_heppy_energyRings_forTraining_%s_legacy_2ptds'%num
+  filename='TTbar_nanoAOD_RegressionPerJet_nanoAOD_2018_%d'%num
 #  filename=filelist[i]
 #  filename='TTbar_nanoAOD_RegressionPerJet_nanoAOD_2017_%d'%num
 #  filename='TTbar_2ptds_RegressionPerJet_heppy_energyRings_forTraining_10_full_2ptd'
--- submit_jobs/2018/find_missing.py
+++ submit_jobs/2018/find_missing.py
@@ -9,7 +9,7 @@ max_num = 166
 for i in range(0,max_num+1) :
 	found = 0
 	num = str(i)
-	search_line = "TTbar_nanoAOD_RegressionPerJet_nanoAOD_2017_"+ num +".root\n"
+	search_line = "TTbar_nanoAOD_RegressionPerJet_nanoAOD_2018_"+ num +".root\n"
 	if search_line not in lines:
 		output.write('%i\n'%i)
 
