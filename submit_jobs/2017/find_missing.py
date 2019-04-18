73e3b803e4390b09dd73365326d1a787dab47577
Nadezda Chernyavskaya (ETHZ) [nchernya] <nchernya@t3ui02.psi.ch>
2019-04-18 15:04:08 +0200

    submitting 2017 with fixed JEC v32

--- submit_jobs/2017/find_missing.py
+++ submit_jobs/2017/find_missing.py
@@ -2,14 +2,14 @@
 
 import string
 
-input = open("present_1.txt","rt")
+input = open("done.txt","rt")
 output = open("missing_1.txt","wt")
 lines = input.readlines()
-max_num = 166
+max_num = 96
 for i in range(0,max_num+1) :
 	found = 0
 	num = str(i)
-	search_line = "TTbar_nanoAOD_RegressionPerJet_nanoAOD_2017_"+ num +".root\n"
+	search_line = "TTbar_nanoAOD_RegressionPerJet_nanoAOD_2017JECv32_"+ num +".root\n"
 	if search_line not in lines:
 		output.write('%i\n'%i)
 
