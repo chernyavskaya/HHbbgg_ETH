97706d1379fd7e8cb363255780e702c88d8aa071
Nadezda Chernyavskaya <nadezda.chernyavskaya@gmail.com>
2020-02-16 14:49:06 +0100

    adding latest changes for training with Mjj

---
+++ scripts/setup_upd_nadya.sh
@@ -0,0 +1,3 @@
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh
+export PYTHONPATH=$PYTHONPATH:/work/$USER/HHbbgg_ETH_devel/Training/python/
+export PYTHONPATH=$PYTHONPATH:/work/$USER/HHbbgg_ETH_devel/ConversionUtils/python/
